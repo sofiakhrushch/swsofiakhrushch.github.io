248fa28e7eadde0a8d0e1ec6b07e974b3b97a1d3
Oleh Blazhko <thestingrayofficial@gmail.com>
2024-11-26 14:42:50 +0200

    Added all necessary files
    Co-Authored-By: sofiakhrushch <185609509+sofiakhrushch@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.tour-item').forEach(item => {
+        const toggleArrow = item.querySelector('.toggle-arrow');
+        const info = item.querySelector('.tour-info');
+
+        toggleArrow.addEventListener('click', () => {
+            if (item.classList.contains('open')) {
+            
+                item.classList.remove('open');
+                info.style.maxHeight = '0'; 
+            } else {
+                
+                document.querySelectorAll('.tour-item.open').forEach(openItem => {
+                    openItem.classList.remove('open');
+                    openItem.querySelector('.tour-info').style.maxHeight = '0'; 
+                });
+
+                item.classList.add('open');
+                info.style.maxHeight = info.scrollHeight + 'px';
+            }
+        });
+    });
+});
